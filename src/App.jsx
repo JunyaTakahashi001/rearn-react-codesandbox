6419fb47a3cd392806e77e4c1a6d5c7f3c2807d2
jyunya-takahashi <67084292+jyunya-takahashi@users.noreply.github.com>
2021-07-31 22:27:47 +0900

    イベント、スタイル設定方法

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  // オブジェクトでstyleを当てる方法
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
+      {/* インラインでstyleを当てる方法 */}
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
